e4951d8a221ecbd9514e41740e0120838f8d660e
ajay.sidal <ajay.sidal@opsvantagedigital.online>
2026-01-20 21:29:19 +0000

    fix: configure path aliases in tsconfig.json

--- components/AccountClientWrapper.tsx
+++ components/AccountClientWrapper.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import ConnectYouTubeButton from './ConnectYouTubeButton'
+import ConnectYouTubeButton from '~/components/ConnectYouTubeButton'
 
 export default function AccountClientWrapper() {
   return <AccountInner />

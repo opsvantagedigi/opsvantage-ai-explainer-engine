8fcb08f18431d80d849c18f39655873a4fd8ae02
ajay.sidal <ajay.sidal@opsvantagedigital.online>
2026-01-21 00:39:00 +0000

    fix: Address localhost connection issue

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7e1353339624f465")
+/******/ 		__webpack_require__.h = () => ("a0b89438f7b4ea89")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f59330b32ad92b7f"; }
+/******/ 		__webpack_require__.h = function() { return "e2d57109e5be5bff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
